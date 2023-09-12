bc8d0b593a67b57468e54a99e885cf6de52a609d
prihutomo <info@prihutomo.web.id>
2023-09-12 11:43:12 +0800

    Update footer.php
    saya rusak dikit

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<!-- partial:partials/_footer.html -->
+<!-- partial:partials/_footer.html
 <footer class="footer">
           <div class="d-sm-flex justify-content-center justify-content-sm-between">
             <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2021.  Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from BootstrapDash. All rights reserved.</span>
